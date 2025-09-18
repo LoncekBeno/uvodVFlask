08585fd0ede77e6bad04d918df4edd4b6d744790
LoncekBeno <167700759+LoncekBeno@users.noreply.github.com>
2025-09-18 10:01:04 +0000

    Website

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #Naredimo strežnik
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import random
 #pip install flask
 app = Flask(__name__)
 
@@ -10,7 +11,15 @@ def home():
 
 @app.route("/CalcLove")
 def calc():
-    print("Calculating love...")
-    return render_template("index.html")
+    print(request.args)
+    req = request.args
+    ime1 = req.get("ime1")
+    ime2 = req.get("ime2")
+    rnd=random.randint(0,100)
+    rez=f"{ime1} in {ime2} se imata rada {rnd}%"
+    if {ime1}=="Beno" or {ime2}=="Beno":
+        print("Beno je zakon")
+    else:
+        return render_template("index.html", rezultat=rez)
 
 app.run(debug = True)
